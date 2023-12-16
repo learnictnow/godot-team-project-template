4757dd3265fdb5e379c80c6927af387bef1bb152
Andrew Thawley <thawleya@staff.cbhs.school.nz>
2023-12-16 22:36:57 +1300

    Splash screen loads menu
    start menu loaded after initial animation

--- TeamMember2/introduction_splash.gd
+++ TeamMember2/introduction_splash.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@export_file("*.tscn") var scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +15,5 @@ func _process(delta):
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "splash":
 		print("Animation Splash finished")
+		get_tree().change_scene_to_file(scene)
 	pass # Replace with function body.
